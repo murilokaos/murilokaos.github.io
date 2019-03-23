9efa76c704c4f1ec3e0397c6ccbac3d72bf2c269
Murilo Henrique <oofleaoo@gmail.com>
2019-03-22 23:52:29 -0300

    pequi test

--- README.md
+++
@@ -1,1 +0,0 @@
-# murilokaos.github.io
---
+++ precache-manifest.289ae52b426bf06c673054e7e13f0c5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "98e9f03d12e41a8ef8ea",
+    "url": "/static/js/main.7f203a5e.chunk.js"
+  },
+  {
+    "revision": "cd2b68106dbe3f2ff620",
+    "url": "/static/js/2.ea2fac77.chunk.js"
+  },
+  {
+    "revision": "98e9f03d12e41a8ef8ea",
+    "url": "/static/css/main.c72a8e91.chunk.css"
+  },
+  {
+    "revision": "78ee8d6239971e36952d55070b55499b",
+    "url": "/index.html"
+  }
+];
